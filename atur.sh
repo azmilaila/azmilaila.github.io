94a7e51927656359d6fe28f3e3561606c76cfce5
Laila Azmi <lailaazmi@my.com>
2021-05-31 12:23:05 +0800

    azmilaila

--- atur.sh
+++ atur.sh
@@ -1,1 +1,15 @@
-./perubahan.sh
+rm -r Packages.bz2
+rm -r Packages.xz
+
+#!/bin/bash
+cd "$(dirname $0)"
+rm -f Packages
+rm -f Packages.xz
+rm -f Packages.zst
+dpkg-scanpackages -m . /dev/null >Packages
+bzip2  Packages
+dpkg-scanpackages debs /dev/null > Packages
+xz -c9 Packages > Packages.xz
+
+git add --all
+git commit -m "azmilaila"
--- hapus.sh
+++
@@ -1,1 +0,0 @@
-rm -r Packages.bz2
