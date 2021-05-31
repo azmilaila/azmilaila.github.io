7c191d99ef9014bf723f987e940438cae15dbc71
Laila Azmi <lailaazmi@my.com>
2021-05-31 12:36:52 +0800

    azmilaila

--- atur.sh
+++ atur.sh
@@ -1,10 +1,16 @@
 rm -r Packages.bz2
 rm -r Packages.xz
 
+#!/bin/bash
+cd "$(dirname $0)"
+rm -f Packages
+rm -f Packages.xz
+rm -f Packages.zst
 dpkg-scanpackages -m . /dev/null >Packages
 bzip2  Packages
 dpkg-scanpackages debs /dev/null > Packages
 xz -c9 Packages > Packages.xz
+rm -f Packages
 
 git add --all
 git commit -m "azmilaila"
