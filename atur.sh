0e5efe1b158d60ebf7bd5288942d59f540769e9e
Laila Azmi <lailaazmi@my.com>
2021-05-31 12:34:49 +0800

    azmilaila

--- atur.sh
+++ atur.sh
@@ -1,10 +1,6 @@
 rm -r Packages.bz2
 rm -r Packages.xz
 
-#!/bin/bash
-cd "$(dirname $0)"
-rm -f Packages.xz
-rm -f Packages.zst
 dpkg-scanpackages -m . /dev/null >Packages
 bzip2  Packages
 dpkg-scanpackages debs /dev/null > Packages
