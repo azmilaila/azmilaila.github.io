7bdbc95453d74a33b3455b610e2d753ff3c5e5f2
Laila Azmi <lailaazmi@my.com>
2021-11-24 01:20:41 +0800

    azmilaila

--- scan.sh
+++ scan.sh
@@ -1,3 +1,26 @@
+#!/bin/bash
+
+echo
+echo "Rebuilding package list..."
+echo "--------------------------"
+
 rm -Rf Packages.bz2
 dpkg-scanpackages -m ./debs > Packages
 bzip2 Packages
+
+echo "--------------------------"
+echo "Done."
+echo
+
+git add --all
+git commit -m "azmilaila"
+
+echo
+
+git push
+
+echo
+
+git pull
+
+
